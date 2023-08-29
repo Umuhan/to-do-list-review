0b25c8d8eef4f587747ff4d7f34f6390a7a0a079
Umu <hanny.ally123@gmail.com>
2023-08-29 12:31:56 +0300

    Modify localStorage.js and index.js

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -1,14 +1,12 @@
 // Function to save tasks to local storage
 function saveTasksToLocalStorage(tasks) {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Function to load tasks from local storage
-function loadTasksFromLocalStorage(tasks) {
-  const storedTasks = localStorage.getItem(tasks);
-  if (storedTasks) {
-    tasks = JSON.parse(storedTasks);
-  }
+function loadTasksFromLocalStorage() {
+  const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  return storedTasks;
 }
 
 export { saveTasksToLocalStorage, loadTasksFromLocalStorage };
